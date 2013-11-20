96118aaf337bae3c4af264bf7611cad8c9a1057a
tbism <yuleibest@gmail.com>
2013-11-20 23:50:00 +0800

    stretch the article content for full width

--- aolreader.user.js
+++ aolreader.user.js
@@ -7,5 +7,6 @@ var css = " @namespace url(http://www.w3.org/1999/xhtml); ";
 
 css += " div.reader-ad-container, div.article-recommended-list { display:none; } "; // hide right bar ads
 css += " div#reader-container {margin-right: 0px} "; // stretch article list to full width
-css += " div#reader-container div.feed-header {right: 0px}";
+css += " div#reader-container div.feed-header {right: 0px}"; // stretch article list header to full width
+css += " div#reader-container div.article-list div.article-content {max-width: none}"; // stretch article content to full width
 GM_addStyle(css);
